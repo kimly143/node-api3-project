3c53829e00f9a6fa22fbe97e26ac51595d4e5c6c
Kim Ly Buck <kim.3993@gmail.com>
2020-06-11 19:24:14 -0500

    set up

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code away!
+const express = require('express');
+const morgan = require('morgan');
+
+const path = require('path');
+
+const server = express();
+
+server.get('/download', (req, res, next) => {
+	const filePath = path.join(__dirname, 'index.html');
+	res.sendFile(filePath, (err) => {
+		if (err) {
+			next(err);
+		} else {
+			console.log('File sent successfully');
+		}
+	});
+});
+
+server.use((err, req, res, next) => {
+	console.error(err);
+
+	res.status(500).json({ message: 'There was an error performing the required operation' });
+});
+server.listen(4040);
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const morgan = require("morgan")
 const server = express();
 
 server.get('/', (req, res) => {
